1132b162ccb8cfd42aa4c9c6571728d8501cdc3f
devanomaly <rafaelfpcosta@gmail.com>
2022-04-18 16:27:20 -0300

    useEffect to fetch data and then render

--- react-redux-module_bundling/robofriends/src/App.js
+++ react-redux-module_bundling/robofriends/src/App.js
@@ -1,18 +1,32 @@
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
-import { useState } from 'react';
-import { robots } from './robots';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 export default function App() {
+  const [currentUserList, setUserList] = useState([{}])
+  const [loaded, setLoaded] = useState(false)
   const [currentSearchField, setSearchField] = useState('')
+
+  useEffect(() => {
+    console.log("useEffect disparou");
+    if (!loaded){
+      (async () => {
+        const resJSON = await (await fetch('https://jsonplaceholder.typicode.com/users')).json();
+        setUserList(resJSON)
+        setLoaded(true)
+      }
+      )();
+    }
+  }, [loaded]) //execute only when didMount
+
   console.log("search for", currentSearchField)
   console.log("renderizou App de novo")
   return (
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
       <SearchBox setSearch={setSearchField} />
-      <CardList robots={robots} searchFields={currentSearchField} />
+      {loaded && <CardList robots={currentUserList} searchFields={currentSearchField} />}
     </div>
   )
 }
--- react-redux-module_bundling/robofriends/src/index.js
+++ react-redux-module_bundling/robofriends/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import { robots } from './robots';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons'
 
@@ -8,7 +9,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <React.StrictMode>
+    {
+      console.log("renderizou index")
+    }
     <App />
   </React.StrictMode>
 );
