251aa6a9b5785ee05cd1b7e708a740744f80ab01
devanomaly <rafaelfpcosta@gmail.com>
2022-04-27 10:12:19 -0300

    edit: melhorando state-ing

--- react-redux-module_bundling/redux-stuff/robofriends_reduxed/src/containers/App.js
+++ react-redux-module_bundling/redux-stuff/robofriends_reduxed/src/containers/App.js
@@ -1,9 +1,15 @@
 import { requestRobots, setSearchField } from '../actions';
 import { connect } from 'react-redux';
 import CardList from '../components/CardList';
+import CardList_2 from '../components/CardList_2';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import { useEffect, useState } from 'react';
+import { searchStringInObjectValues } from '../services/string-in-object-vals'
+
+import {
+  useEffect,
+  // useState
+} from 'react';
 import './App.css'
 
 const mapStateToProps = state => ({
@@ -12,31 +18,30 @@ const mapStateToProps = state => ({
   isPending: state.requestRobots.isPending,
   error: state.requestRobots.error,
 })
+
 const mapDispatchToProps = dispatch => ({
   onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
   onRequestRobots: () => dispatch(requestRobots())
 })
 
 function App(props) {
-  const [loaded, setLoaded] = useState(false)
-  const { searchField, onSearchChange, robots, onRequestRobots } = props
+  // const [loaded, setLoaded] = useState(false)
+  const { searchField, onSearchChange, robots, onRequestRobots, isPending } = props
+  console.log("render App")
+  const filteredRobots = robots.filter(robot => searchStringInObjectValues(searchField, robot))
 
   useEffect(() => {
-    // console.log("useEffect disparou");
-    // console.log("store state\n", store.getState())
-    if (!loaded) {
-      onRequestRobots()
-      setLoaded(true)
-    }
-  }, [loaded]) //execute only when didMount
+    onRequestRobots()
+  },
+    []
+  ) //execute only when didMount
 
-  // console.log("renderizou App de novo")
 
   return (
     <div className='tc'>
       <h1 className='f1'>RoboFriends</h1>
       <SearchBox onSearchChange={onSearchChange} />
-      {loaded ? <Scroll><CardList robots={robots} searchFields={searchField} /></Scroll> : <h1>Loading...</h1>}
+      {isPending ? <h1>Loading...</h1> : <Scroll><CardList_2 currentRobots={filteredRobots} /></Scroll>}
     </div>
   )
 }
